8355257c2991bf5949f4d5f6875e60d984d3c926
fishwickid <chadfishwick@gmail.com>
2020-09-26 16:24:52 +0800

    project cards

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,11 +1,64 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardText,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+} from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = { activeTab: 0 };
   }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background: "url(https://reactjs.org/logo-og.png) center / cover",
+            }}
+          >
+            Javascript Project #1
+          </CardTitle>
+          <CardText>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </CardText>
+          <CardActions border>
+            <button coloured>GitHub</button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h2>This is Backend</h2>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h2>This is React</h2>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="category-tabs">
@@ -18,6 +71,14 @@ class Projects extends Component {
           <Tab>Backend</Tab>
           <Tab>React</Tab>
         </Tabs>
+
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }
